5bb044e7c3f06a5dbfc0c9660d03b66ec88a967d
Jay Dwived <jayraj1999@gmail.com>
2022-01-30 21:18:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebaserealtime/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.firebaserealtime
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            var name : String = findViewById<EditText>(R.id.etFullName).text.toString()
+            val re = Regex("[^A-Za-z0-9 ]")
+            name = re.replace(name, "") // works
+
+            val intent = Intent(this , MapsActivity::class.java)
+            intent.putExtra("MFullName" , name)
+            startActivity(intent)
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/firebaserealtime/MapsActivity.kt
@@ -0,0 +1,174 @@
+package com.example.firebaserealtime
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.firebaserealtime.databinding.ActivityMapsBinding
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest : LocationRequest
+    private lateinit var database : FirebaseDatabase
+    private lateinit var fullName : String
+    private lateinit var myRef : DatabaseReference
+
+
+    val locationCallback = object : LocationCallback(){
+
+        override fun onLocationResult(locationResult: LocationResult) {
+            for (i in locationResult.locations){
+
+                //Log.d("TAG", "onLocationResult:  $i ")
+                //val date = DateFormat.getDateInstance().format("MMMM d, yyyy ")
+
+                val date: String = SimpleDateFormat("yyyy-MM-dd").format(Date())
+                val cTime: String = SimpleDateFormat("HH:mm:ss:SSS").format(Date())
+
+                myRef = myRef.child(date).child(cTime)
+
+                myRef.child("Latitude").setValue(i.latitude)
+                myRef.child("Longitude").setValue(i.longitude)
+
+            }
+            super.onLocationResult(locationResult)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        locationRequest = LocationRequest.create()
+        locationRequest.apply {
+            setInterval(2000)
+            setFastestInterval(500)
+            setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        }
+
+        getLocationFineOnce()
+        val myRef = database.getReference(fullName)
+
+        binding.apply {
+            startSyncButton.setOnClickListener { checkSettingsThenStart() }
+            stopSyncButton.setOnClickListener { stopLocationUpdates() }
+        }
+
+        fullName = intent.getStringExtra("MFullName")!!
+    }
+
+    private fun startLocationUpdates(){
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+
+            Toast.makeText(this, "updates Location no permission", Toast.LENGTH_SHORT).show()
+            return
+
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest , locationCallback , Looper.getMainLooper())
+    }
+
+    fun getLocationFineOnce(){
+        //Toast.makeText(this, "get Fine called", Toast.LENGTH_SHORT).show()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this , arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION //, Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) , 101)
+            //finish()
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        val task = fusedLocationClient.lastLocation
+
+        task.addOnSuccessListener {
+            if(it!=null) {
+                //Toast.makeText(this, "${it.latitude} , ${it.longitude}", Toast.LENGTH_SHORT).show()
+                //mapActivity()
+
+                val sydney = LatLng(it.latitude, it.longitude)
+                mMap.addMarker(MarkerOptions().position(sydney).title("Your Location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            }
+            else
+                Toast.makeText(this, "Error , it = null", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkSettingsThenStart(){ // Continuous Location
+        val request = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest).build()
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val client = LocationServices.getSettingsClient(this);
+        val task : Task<LocationSettingsResponse> = client.checkLocationSettings(request)
+
+        val successListener = object : OnSuccessListener<LocationSettingsResponse> {
+            override fun onSuccess(p0: LocationSettingsResponse?) {
+                startLocationUpdates()
+            }
+        }
+
+        val failureListener = object : OnFailureListener {
+            override fun onFailure(p0: Exception) {
+                Log.e("Failed", "onFailureListener: " )
+            }
+        }
+
+        task.addOnSuccessListener (successListener)
+        task.addOnFailureListener (failureListener)
+    }
+
+
+    private fun stopLocationUpdates(){
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+}
